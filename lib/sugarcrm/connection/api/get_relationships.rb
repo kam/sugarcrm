c460a4b03db1ef544d71be3cf21d6298d4d50a60
Khamsouk Souvanlasy <khamsouk@me.com>
2014-12-16 15:58:07 +1100

    resolve_related_fields already returns as JSON

--- lib/sugarcrm/connection/api/get_relationships.rb
+++ lib/sugarcrm/connection/api/get_relationships.rb
@@ -1,19 +1,19 @@
 module SugarCRM; class Connection
-  # Retrieves a collection of beans that are related 
-  # to the specified bean and, optionally, returns 
+  # Retrieves a collection of beans that are related
+  # to the specified bean and, optionally, returns
   # relationship data
   # Ajay Singh --> changed as per our equirment.
   # "related_fields": #{resolve_related_fields(module_name, related_to)},
 
   def get_relationships(module_name, id, related_to, opts={})
     login! unless logged_in?
-    options = { 
+    options = {
       :query => '',
-      :fields => [], 
+      :fields => [],
       :link_fields => [],
-      :related_fields => [], 
+      :related_fields => [],
       :deleted => 0
-    }.merge! opts  
+    }.merge! opts
 
     json = <<-EOF
       {
@@ -22,7 +22,7 @@ module SugarCRM; class Connection
         "module_id": "#{id}",
         "link_field_name": "#{related_to.downcase}",
         "related_module_query": "#{options[:query]}",
-        "related_fields": #{((options and options[:related_fields].present?) ? options[:related_fields] : resolve_related_fields(module_name, related_to) ).to_json},     
+        "related_fields": #{(options and options[:related_fields].present?) ? options[:related_fields].to_json : resolve_related_fields(module_name, related_to)},
         "related_module_link_name_to_fields_array": #{options[:link_fields].to_json},
         "deleted": #{options[:deleted]}
       }
